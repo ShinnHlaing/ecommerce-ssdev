879b16c1bcfc23676c8e03c1a42f107eb66c8fe5
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-02-05 00:13:59 +0630

    modify files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import { Checkout } from './pages/Checkout'
+import { Orders } from './pages/Orders'
+import { Tracking } from './pages/Tracking'
 import './App.css'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path='checkout' element={<Checkout />} />
+      <Route path='orders' element={<Orders />} />
+      <Route path='tracking' element={<Tracking />} />
     </Routes>
   )
 }
