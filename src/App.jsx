e27c58f4aef2dd6b9cc08e842c8596a91ad80a61
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-02-04 23:28:27 +0630

    intsall react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import './App.css'
 
 function App() {
   return (
-    <Home />
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path='checkout' element={<div>Checkout Page</div>} />
+    </Routes>
   )
 }
 
