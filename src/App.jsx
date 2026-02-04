11c6b6f4e3af9403efa8682d3c9413a975f08af9
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-02-04 23:38:46 +0630

    add: checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
+import { Checkout } from './pages/Checkout'
 import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path='checkout' element={<div>Checkout Page</div>} />
+      <Route path='checkout' element={<Checkout />} />
     </Routes>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import "./header.css"
 export default function Home() {
     return (
         <>
+            <title>Ecommerce Project</title>
             <div className="header">
                 <div className="left-section">
                     <a href="index.html" className="header-link">
